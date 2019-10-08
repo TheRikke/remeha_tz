2a0c8ad584a07aaa39d4de09189a8efbfe593129
rikky <rikky@web.de>
2019-10-08 17:15:48 +0200

    Add setup.py for simpler packanging and installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='remeha_tz',
+    version='0.1',
+    packages=[],
+    url='',
+    license='',
+    author='rikky',
+    author_email='',
+    python_requires='>=3',
+    description='', install_requires=['dash', 'plotly', 'pyserial', 'paho-mqtt']
+)
